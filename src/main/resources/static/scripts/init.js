71976dd8478fbf73ad77537cdd1691f0d96176ea
chanhlt <chanhlt190290@gmail.com>
2018-05-06 14:25:28 +0900

    remove some info

--- src/main/resources/static/scripts/init.js
+++ src/main/resources/static/scripts/init.js
@@ -1,11 +1,4 @@
 var config = {
-    apiKey: "AIzaSyD-EFzXKF4uELsQiI1smzv5FtiavfkQF4E",
-    authDomain: "test-app-fd08f.firebaseapp.com",
-    databaseURL: "https://test-app-fd08f.firebaseio.com",
-    projectId: "test-app-fd08f",
-    storageBucket: "test-app-fd08f.appspot.com",
-    messagingSenderId: "1031328655061",
-    clientId: '682572018798-4rulmci9ecq7h6nmp2ljo3v3doomaq88.apps.googleusercontent.com',
     scopes: [
         "email",
         "profile",
